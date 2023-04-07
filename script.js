e9028c81557a0ba36a7fbd7f47d8b3241331f61d
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-04-08 01:02:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	getName(){
+		return "This user name"+this.name;
+	}
+	setName(){
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(this.name+"is studying");
+	}
+	
+}
 
 class Teacher extends Person {}
-
+teach(){
+	console.log(this.name+"is teaching");
+}
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
